7423a74f60b23953a60dba05e8f66cdb77765b52
Kunal Shah <kunalashokshah@gmail.com>
2011-07-15 16:29:24 -0400

    set hosts

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,8 @@ from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd
 from fabric.contrib.console import confirm
 
+env.hosts = ['kunal@kunalashah.com']
+
 def deploy():
   code_dir = '/var/www/'
   with cd(code_dir):
