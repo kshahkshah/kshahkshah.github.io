f9ee2afeed4f0481035f370c557d7d96952ff8e3
Kunal Shah <kunalashokshah@gmail.com>
2011-07-15 16:30:23 -0400

    Import everythin'

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import *
 from fabric.contrib.console import confirm
 
 env.hosts = ['kunal@kunalashah.com']
