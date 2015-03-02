e785b8367c066963c281f5a1d95db8be365ec82c
Kunal Shah <me@kunalashah.com>
2015-03-01 20:47:22 -0500

    WIP

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ get '/' do
   erb :index
 end
 
+get '/invite' do
+  response['Cache-Control'] = "public, max-age=86400"
+  erb :invite
+end
+
 get '/cv' do
   response['Cache-Control'] = "public, max-age=86400"
   erb :cv
