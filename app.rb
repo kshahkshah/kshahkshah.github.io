5fa7f0f40a8c56008f7ddc041e980231a81a4338
Kunal Shah <me@kunalashah.com>
2015-03-03 01:56:42 -0500

    temporary about page

--- app.rb
+++ app.rb
@@ -1,16 +1,1 @@
 require 'sinatra' 
-
-get '/' do
-  response['Cache-Control'] = "public, max-age=86400"
-  erb :index
-end
-
-get '/invite' do
-  response['Cache-Control'] = "public, max-age=86400"
-  erb :invite
-end
-
-get '/cv' do
-  response['Cache-Control'] = "public, max-age=86400"
-  erb :cv
-end
